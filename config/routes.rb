7c61eaa090a857dfc64d33b49ca884e553882b03
maximo954 <agphotography954@gmail.com>
2016-06-02 01:43:40 -0400

    Update view and portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'user/registrations'}
   resources :user_stocks, except: [:show, :edit, :update]
-  
+
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
